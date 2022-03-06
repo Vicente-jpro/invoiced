52f2da2c1169a756111377e3f34b80472593fc18
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-03-06 18:10:41 +0100

    Controller actions

--- app/controllers/v1/contacts_controller.rb
+++ app/controllers/v1/contacts_controller.rb
@@ -1,2 +1,43 @@
 class V1::ContactsController < ApplicationController
+    def index 
+      @contacts = Contact.order("created_at DESC")
+      render json: { status: "SUCCESS", message: "Loaded Contacts", data: @contacts}, status: :ok
+    end
+
+    def create 
+      @contact = Contact.new(contact_params)
+      if @contact.save
+        render json:{status: "SUCCESS", message: "Created Successfully", data: @contact}, status: :ok
+      else  
+        render json: {status: "ERROR", message: "Error to create a contact", data: @contact.errors}, status: :unprocessable_entity 
+      end
+    end
+
+    def destroy
+      @contact = Contact.find(params[:id])
+      if @contact.destroy
+        render json: {status: "SUCCESS", message: "Deleted contact", data: @contact}, status: :ok 
+      else
+        render json: {status: "ERROR", message: "Error deleting contact", data: @contact.erros}, status: :ok 
+      end
+    end
+
+    def update 
+    end
+
+    def show 
+      @contact = Contact.find(params[:id])
+      debugger
+      if @contact
+        render json: { status: "SUCCESS", message: "Loaded Contact", data: @contact}, status: :ok
+      else  
+        render json: { status: "ERROR", message: "Contact not found", data: @contact}, status: :ok
+      end
+    end
+
+    private 
+        def contact_params
+          params.require(:contact).permit(:first_name, :last_name, :email)
+        end
+
 end
