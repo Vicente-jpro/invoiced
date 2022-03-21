6920e814777d6937fc025c32e78cef56b116e1c6
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-03-21 21:39:59 +0100

    default format added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  namespace :api do 
+  namespace :api, defaults: {format: :json} do 
     resources :contacts
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
